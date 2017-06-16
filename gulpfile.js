47a88184f4b7cf42f5207baacf490acc6040ea85
DeeSouza <dee-souza@hotmail.com>
2017-06-16 11:41:18 -0300

    Minify JS

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,12 @@ var gulp   			= require('gulp');
 var gulpsass		= require('gulp-sass');
 var gulpif 			= require('gulp-if');
 var autoprefixer 	= require('gulp-autoprefixer');
+var minify 			= require('gulp-minify');
 
 gulp.task('sass', function(){
 	return gulp.src('assets/sass/**/*')
 	.pipe(gulpif(function(file){ return file.path.indexOf('.scss') != -1; }, gulpsass()))
 	.pipe(gulp.dest('css/'));
-});
-
-gulp.task("default", function(){
-	gulp.watch("assets/**/*.scss", ['sass', 'run-prefixer']);
 });
 
 gulp.task('run-prefixer', ['sass'], function() {
@@ -20,4 +17,22 @@ gulp.task('run-prefixer', ['sass'], function() {
 		cascade: true
 	}))
 	.pipe(gulp.dest('css/'));
+});
+
+gulp.task('compress', function() {
+  gulp.src('assets/js/*.js')
+    .pipe(minify({
+        ext:{
+            src: '-debug.js',
+            min:'-min.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.min.js']
+    }))
+    .pipe(gulp.dest('dist/min'))
+});
+
+gulp.task("default", function(){
+	gulp.watch("assets/**/*.scss", ['sass', 'run-prefixer']);
+	gulp.watch("assets/**/*.js", ['compress']);
 });
